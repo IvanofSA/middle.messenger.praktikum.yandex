e62f681d17597c27ccc0efd012a47d5ce5cb0d4b
s.ivanov <sergey.ivanov@karuna.group>
2021-08-22 12:55:36 +0300

    sprint 3

--- src/pages/signin/signin.ts
+++ src/pages/signin/signin.ts
@@ -1,11 +1,11 @@
-import { compile } from "pug";
+import {compile} from "pug";
 import Block from "../../utils/Block/Block";
 import PageModel from "../../constans/page.model";
-import { multiValidate } from "../../utils/validate";
-import { Input } from "../../components/input";
-import { Link } from "../../components/link";
-import { Button } from "../../components/button";
-import { authAPI } from "../../api/authApi";
+import {multiValidate} from "../../utils/validate";
+import {Input} from "../../components/input";
+import {Link} from "../../components/link";
+import {Button} from "../../components/button";
+import {authAPI} from "../../api/authApi";
 import router from "../../index";
 
 const template: string = `
@@ -21,76 +21,89 @@ const template: string = `
 const compileTemplate = compile(template);
 
 export default class SignIn extends Block {
-  constructor(props: PageModel) {
-    const loginField = new Input({
-      theme: "login",
-      label: "Логин",
-      id: "loginField",
-      placeholder: "Введите логин",
-      name: "login",
-      type: "text",
-      value: "",
-      disabled: false,
-      error: "Ошибка",
-    });
-    const password = new Input({
-      theme: "login",
-      label: "Пароль",
-      id: "password",
-      placeholder: "Введите пароль",
-      name: "password",
-      type: "password",
-      value: "",
-      disabled: false,
-      error: "Ошибка",
-    });
-    const link = new Link({
-      text: "Нет аккаунта?",
-      classNames: ["form__link"],
-      href: "/sign-up",
-    });
-    const button = new Button({
-      text: "Авторизоваться",
-      classNames: ["form__enter"],
-      type: "submit",
-      disabled: false,
-    });
-    const events = {
-      signin: {
-        eventName: "submit",
-        tagEvent: "form",
-        callback: (e) => {
-          e.preventDefault();
-          const isValid = multiValidate(e.target, "signin");
-          if (isValid) {
-            const data = new FormData(e.target);
-            authAPI
-              .signIn(data)
-              .then(() => {
-                router.go("/messenger");
-              })
-              .catch((err) => {
-                console.error(err);
-              });
-          }
-        },
-      },
-    };
-    super({
-      tagName: "main",
-      classNames: ["login", "main"],
-      children: {
-        loginField,
-        password,
-        link,
-        button,
-      },
-      ...props,
-      events,
-    });
-  }
+    constructor(props: PageModel) {
+        const loginField = new Input({
+            theme: "login",
+            label: "Логин",
+            id: "loginField",
+            placeholder: "Введите логин",
+            name: "login",
+            type: "text",
+            value: "",
+            disabled: false,
+            error: "Ошибка",
+        });
+        const password = new Input({
+            theme: "login",
+            label: "Пароль",
+            id: "password",
+            placeholder: "Введите пароль",
+            name: "password",
+            type: "password",
+            value: "",
+            disabled: false,
+            error: "Ошибка",
+        });
+        const link = new Link({
+            text: "Нет аккаунта?",
+            classNames: ["form__link"],
+            href: "/sign-up",
+        });
+        const button = new Button({
+            text: "Авторизоваться",
+            classNames: ["form__enter"],
+            type: "submit",
+            disabled: false,
+        });
+        const events = {
+            signin: {
+                eventName: "submit",
+                tagEvent: "form",
+                callback: (e) => {
+                    e.preventDefault();
+                    const isValid = multiValidate(e.target, "signin");
+                    if (isValid) {
+                        const data = new FormData(e.target);
+                        authAPI
+                            .signIn(data)
+                            .then(() => {
+                                router.go("/messenger");
+                            })
+                            .catch((err) => {
+                                console.error(err);
+                            });
+                    }
+                },
+            },
+        };
+        super({
+            tagName: "main",
+            classNames: ["login", "main"],
+            children: {
+                loginField,
+                password,
+                link,
+                button,
+            },
+            ...props,
+            events,
+        });
+    }
 
-  render(): string {
-    return compileTemplate(this.props);
-  }
+    componentDidMount(): void {
+        authAPI
+            .getUserInfo()
+            .then((data) => {
+                console.log(data)
+                router.go("/messenger");
+            })
+            .catch((er) => {
+                console.error(er);
+            });
+    }
+
+    render(): string {
+
+        return compileTemplate(this.props);
+    }
 }
